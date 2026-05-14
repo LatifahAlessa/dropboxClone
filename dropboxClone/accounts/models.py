433f74f049ed183fcda1b7bf38e976eafa8ce828
LatifahAlessa <latifhaleisa@gmail.com>
2026-05-14 12:55:21 +0300

    remove generate_uuid wrapper, use uuid7 directly

--- dropboxClone/accounts/models.py
+++ dropboxClone/accounts/models.py
@@ -2,9 +2,6 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from uuid_extensions import uuid7
 
-
-def generate_uuid():
-    return uuid7()
     
 class CustomUser(AbstractUser):
     id = models.UUIDField(primary_key=True, default=uuid7, editable=False)
